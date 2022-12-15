6cfc29eebeedfb7fb13652bc4702bb5ca1ae5b69
Olga <olga.veselova2001@gmail.com>
2022-12-14 22:29:26 -0500

    Flashcards animation created; updated different styles, fixed stuff up

--- static/js/js.js
+++ static/js/js.js
@@ -1,1 +1,11 @@
+$('.back').hide();
 
+$('.front', '.flashcard').hover(function() {
+    $(this).hide();
+    $(this).siblings('.back').addClass( "animated speed" ).show();
+});
+
+$('.back', '.flashcard').mouseleave(function() {
+  $(this).hide();
+  $('.front').addClass( "animated speed" ).show();
+});
