5a2e6971ac5f5c49e57a2022eae95a02d5a4752a
vinny2001 <30963796+vinny2001@users.noreply.github.com>
2022-12-15 00:25:54 -0500

    Final Commit (?)

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = ['static']
-SSTATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
